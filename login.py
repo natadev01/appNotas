4840bfefec47c7340aa48644d92ed1c70ae6b10f
natadev01 <96508629+natadev01@users.noreply.github.com>
2022-03-10 19:04:41 -0400

    Add files via upload

--- login.py
+++ login.py
@@ -19,27 +19,28 @@ def login():
 
     os.system ("cls")
 
-    cursor.execute('SELECT user  FROM registro')
-    regisUser = cursor.fetchall()
-    cursor.execute('SELECT passw FROM registro where user=?',(name,))
-    regisPass = cursor.fetchall()
-    regisPass = regisPass[0][0]
-    conex.close()
+    try:
+        cursor.execute('SELECT passw FROM registro where user=?',(name,))
+        regisPass = cursor.fetchall()
+        regisPass = regisPass[0][0]
+        conex.close()
     # print(regisUser)
     # print(regisPass)
     # time.sleep(10)
-   
-    if (name,)  in regisUser and (passw) in regisPass:
+
+        if passw == regisPass:
+            if  manageNotes.manageNotes(name) == True:
+                return True
+            else:
+                return False
+        print('Contraseña incorrecta\nIntente de nuevo!!')
+        time.sleep(5)
+        return False
         
-        if  manageNotes.manageNotes(name) == True:
-            return True
-        else:
-            return False
         
-        
-       
-    print('Usuario o contraseña incorrecta\nIntente de nuevo!!') 
-    time.sleep(5)
-    return False
+    except:   
+        print('Usuario incorrecta\nIntente de nuevo!!') 
+        time.sleep(5)
+        return False
 
     
